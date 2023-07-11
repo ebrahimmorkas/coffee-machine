bdfa261ec120ff578617e9315a9f2766ce5ae3b3
Ebrahim <morkasebrahim3@gmail.com>
2023-07-11 18:30:45 +0530

    User can give money and take the remaining money if applicable and cant get coffe if not enough money

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 water = 300
 milk = 200
 coffee = 100
-money = 0
+money_in_coffee_machine = {
+    "10": 10,
+    "20": 10,
+    "50": 5,
+    "100": 5,
+    "200": 3
+}
 
 menu = {
     "espresso": {
@@ -39,7 +45,7 @@ def available_resources():
     print(f"Water: {water} ml")
     print(f"Milk: {milk} ml")
     print(f"Coffee: {coffee} g")
-    print(f"$: {money}")
+    # print(f"$: {money}")
 
 while True:
     user_choice = input("What would you like (espresso/latte/cappuccino) or enter 'report' to know the available ingredients\n")
@@ -58,6 +64,43 @@ while True:
             coffee = menu[user_choice]['ingredients']['coffee']
 
             print(f"Latte will cost {menu[user_choice]['cost']}")
+
+            # Creating the list that will contain the number of notes entered by user when asked
+            number_of_notes_entered = []
+
+            # Creating the variable that will keep track of total money entered by user
+            total = 0
+
+            money_input = input("Enter how many coins or notes of RS 10 you have entered")
+            number_of_notes_entered.append(int(money_input))
+            total = total + 10 * int(money_input)
+            money_input = input("Enter how many coins or notes of RS 20 you have entered")
+            total = total + 20 * int(money_input)
+            number_of_notes_entered.append(int(money_input))
+            money_input = input("Enter how many notes of RS 50 you have entered")
+            total = total + 50 * int(money_input)
+            number_of_notes_entered.append(int(money_input))
+            money_input = input("Enter how many notes of RS 100 you have entered")
+            total = total + 100 * int(money_input)
+            number_of_notes_entered.append(int(money_input))
+            money_input = input("Enter how many notes of RS 200 you have entered")
+            total = total + 200 * int(money_input)
+            number_of_notes_entered.append(int(money_input))
+
+            # Checking whether the amount entered by user exactly matches the amount of coffee
+            if total == menu[user_choice]['cost']:
+                print("Exact money entered")
+
+                # Iterating the dictionary that contains the amount of each note present in the machine and them updating it when user has entered the money that is increasing the amount of notes by number of notes entered by the user
+                iterator = 0
+                for i in money_in_coffee_machine:
+                    money_in_coffee_machine[i] = money_in_coffee_machine[i] + number_of_notes_entered[iterator]
+                    iterator = iterator + 1
+            elif total > menu[user_choice]['cost']:
+                money_remaining = total - menu[user_choice]['cost']
+                print(f"Take your change {money_remaining}")
+            else:
+                print("You dont have enough money")
         else:
             # Resources are not available
             print("Resources Not available")
