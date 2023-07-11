4ced85d2aee063992bfa692f9741a8aee7836a47
Ebrahim <morkasebrahim3@gmail.com>
2023-07-11 17:19:04 +0530

    Coffee is being produced virtually by checking the available resources and if resources not available it will display message no pricing added in this version

--- main.py
+++ main.py
@@ -28,9 +28,12 @@ menu = {
             "water": 50,
             "coffee": 150
         },
-        "cost": 200
+        "cost": 300
     }
 }
+
+# List that will contain all the names of available coffees
+available_coffees = ['espresso', 'latte', 'cappuccino']
 
 def available_resources():
     print(f"Water: {water} ml")
@@ -42,3 +45,21 @@ while True:
     user_choice = input("What would you like (espresso/latte/cappuccino) or enter 'report' to know the available ingredients\n")
     if user_choice == 'report':
         available_resources()
+    elif user_choice in available_coffees:
+        # print("Coffee Available")
+        # print(menu["latte"]['ingredients']['milk'])
+
+        # Checking whether the resources are available
+        if menu[user_choice]['ingredients']['milk'] <= milk and menu[user_choice]['ingredients']['water'] <= water and menu[user_choice]['ingredients']['coffee'] <= coffee:
+            # Resources are available
+            # print("Resources Available")
+            milk = milk - menu[user_choice]['ingredients']['milk']
+            water = water - menu[user_choice]['ingredients']['water']
+            coffee = menu[user_choice]['ingredients']['coffee']
+
+            print(f"Latte will cost {menu[user_choice]['cost']}")
+        else:
+            # Resources are not available
+            print("Resources Not available")
+    else:
+        print("Coffee not available")
